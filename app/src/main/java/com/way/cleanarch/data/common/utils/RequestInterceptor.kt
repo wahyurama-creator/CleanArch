74304e6db85382afc407f25553ac4d6f1d79b682
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-09-25 23:11:52 +0700

    Create RequestInterceptor.kt for add headers Auth

---
+++ app/src/main/java/com/way/cleanarch/data/common/utils/RequestInterceptor.kt
@@ -0,0 +1,13 @@
+package com.way.cleanarch.data.common.utils
+
+import com.way.cleanarch.infra.utils.SharedPrefs
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class RequestInterceptor constructor(private val pref: SharedPrefs) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = pref.getToken()
+        val newRequest = chain.request().newBuilder().addHeader("Authorization", token).build()
+        return chain.proceed(newRequest)
+    }
+}
