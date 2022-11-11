5c675e57fbe7557213df1cf4bef125cd54a27887
wahyu.rama <wahyu.rama@gramedia.id>
2022-11-12 01:21:08 +0700

    Create design sign in

---
+++ app/src/main/java/com/way/cleanarch/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.way.cleanarch.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.way.cleanarch.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
